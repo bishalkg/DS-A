48f1766d4742d30606eb55799ee77f0d57e48779
Bishal Gautam <bgautam.se@gmail.com>
2021-11-10 15:10:53 -0800

    bfs and dfs with graph

--- src/DS/Graph.js
+++ src/DS/Graph.js
@@ -11,7 +11,6 @@ class Graph {
     }
 
   }
-
   addEdge(node1, node2) {
     //undirected graph
     //if node1/node2 exists in the graph
@@ -25,3 +24,94 @@ class Graph {
 
   }
 }
+
+
+//fireship Graph
+
+const airports = 'PHX BKK OKC JFK MEX EZE HEL LOS LAX LIM'.split(' ');
+
+const routes = [
+  ['PHX', 'LAX'],
+  ['PHX', 'JFK'],
+  ['JFK', 'OKC'],
+  ['JFK', 'HEL'],
+  ['JFK', 'LOS'],
+  ['MEX', 'LAX'],
+  ['MEX', 'BKK'],
+  ['MEX', 'LIM'],
+  ['MEX', 'EZE'],
+  ['LIM', 'BKK'],
+];
+
+
+//use an adjacency list instead of a matrix bc would take up lot of space
+
+//the graph
+const adjacencyList = new Map();
+
+function addNode(airport) {
+  adjacencyList.set(airport, []);
+}
+
+//add edge, undirected
+function addEdge(origin, destination) {
+  adjacencyList.get(origin).push(destination);
+  adjacencyList.get(destination).push(origin);
+}
+
+airports.forEach(addNode); //shorthand, passes in each airport to addNode
+routes.forEach(route => addEdge(...route)); //takes in both strings in route array
+
+// console.log(adjacencyList);
+
+//BFS
+function bfs(start, target) {
+
+  const visited = new Set();
+  const queue = [start];
+  while (queue.length > 0) {
+    const airport = queue.shift();
+
+    const destinations = adjacencyList.get(airport); //grab all edges, so that they can added to queue
+
+    for (const destination of destinations) {
+
+
+      if (destination === target) {
+        return true;
+      }
+
+      //in order to prevent duplicates and infinite loop
+      if (!visited.has(destination)) {
+        visited.add(destination);
+        queue.push(destination);  //item only gets enqueued if it has not been visited
+      }
+    }
+  }
+  return false;
+}
+
+console.log(bfs('PHX', 'BKK'));
+
+
+//DFS
+function dfs(start, target, visited = new Set()) {
+
+  visited.add(start);
+  const destinations = adjacencyList.get(start);
+  for (const destination of destinations) {
+    if (destination === target) {
+      return true;
+    }
+
+    if (!visited.has(destination)) {
+      return dfs(destination, target, visited);
+    }
+  }
+  return false;
+}
+
+console.log(dfs('PHX', 'BKK'));
+
+
+//O(V + E)   total number of nodes + edges, scales linearly with addition of vertices or edges
