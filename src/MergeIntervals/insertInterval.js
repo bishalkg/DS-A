19e7cec296c85dfb1a3b654583abcc4e656be3a9
Bishal Gautam <bgautam.se@gmail.com>
2021-12-08 16:43:14 -0800

    insert interval optimal solution

---
+++ src/MergeIntervals/insertInterval.js
@@ -0,0 +1,96 @@
+/*
+57. Insert Interval
+Medium
+
+3941
+
+296
+
+Add to List
+
+Share
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+*/
+
+//fastest solution O(N) time and space
+var insert = function(intervals, newInterval) {
+  var result = [];
+  var i = 0;
+
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) { //while intervals are less than the new interval, push them to result
+                                                                      //we will stop where the next interval can be merged, or starts a new interval
+      result.push(intervals[i]);
+      i++;
+  }
+
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) { //while the start of the current interval is less than the end of the newInterval, merge
+      newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+      newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+      i++;
+  }
+
+  result.push(newInterval);
+
+  while (i < intervals.length) {
+      result.push(intervals[i]);
+      i++;
+  }
+  return result;
+}
+
+
+
+
+//naiive slow solution, push newInterval into intervals and run mergeIntervals
+var insert = function(intervals, newInterval) {
+  intervals.push(newInterval);
+  intervals.sort((a, b) => a[0]-b[0]);
+  var currInt = intervals[0];
+  var merged = [currInt];
+  for (var i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] <= currInt[1]) {
+          currInt[1] = Math.max(currInt[1], intervals[i][1]);
+      } else {
+          currInt = intervals[i];
+          merged.push(currInt);
+      }
+  }
+  return merged;
+};
+
+
+//another slow solution
+var insert = function(intervals, newInterval) {
+  var result = [];
+  var onLeft = false;
+
+  for (var i = 0; i < intervals.length; i++) {
+      if (newInterval[1] < intervals[i][0]) {
+          result.push(newInterval);
+          onLeft = true;
+          break;
+      } else if (newInterval[0] > intervals[i][1]) {
+          result.push(intervals[i]);
+      } else {
+          newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])]
+      }
+  }
+
+  if (onLeft) {
+      return result.concat(intervals.splice(i));
+  }
+
+  result.push(newInterval);
+  return result;
+};
---
+++ src/MergeIntervals/mergeIntervals.js
@@ -0,0 +1,74 @@
+/*
+56. Merge Intervals
+Medium
+
+10634
+
+457
+
+Add to List
+
+Share
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+
+*/
+
+var merge = function(intervals) {
+  if (intervals.length < 2) return intervals;
+  intervals.sort((a, b) => a[0] - b[0]); //sort by start index of each interval
+
+  var start = intervals[0][0];  //start with the start of the first interval, guaranteed to be the smallest bc is sorted
+  var end = intervals[0][1];    //and start end with the end of the first interval --- this will be reassign to grow the interval
+  var merged = [];              //merged intervals to return
+  for (var i = 1; i < intervals.length; i++ ) { //now start with the second interval, and loop through all intevals
+      if (intervals[i][0] <= end) {   //if the start of the next interval is less or equal to the end of the first, it means the first overlaps the second
+          end = Math.max(end, intervals[i][1]); //so we can reassign the end pointer to be the max of the two interval's end values
+      } else {
+          merged.push([start, end]);  //if there were no overlaps, push the current merged interval ([start, end]) into our result array
+          start = intervals[i][0];  //and reset the starting and ending intervals
+          end = intervals[i][1];
+      }
+  }
+  merged.push([start, end]);    //we need to push the last interval into the result array since the loop will end before this happens
+  return merged;
+};
+
+
+
+//similar solution except we mutate the interval while its within the result array
+var merge = function(intervals) {
+  if (intervals.length < 2) return intervals;
+  intervals.sort((a, b) => a[0] - b[0]);
+  var currInterval = intervals[0];
+  var merged = [currInterval];
+
+  for (var i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] <= currInterval[1]) {
+          currInterval[1] = Math.max(currInterval[1], intervals[i][1]);
+      } else {
+          currInterval = intervals[i]; //reassign to the new interval
+          merged.push(currInterval);  //push that interval into the result array and repeat...
+      }
+  }
+  return merged;
+};
