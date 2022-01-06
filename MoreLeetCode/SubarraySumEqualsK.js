82a8a685d20bef69fdecd23bfc5da5ce9b607e85
Bishal Gautam <bgautam.se@gmail.com>
2022-01-06 12:36:37 -0800

    optimized solution for subarraysumequalsK

--- MoreLeetCode/SubarraySumEqualsK.js
+++ MoreLeetCode/SubarraySumEqualsK.js
@@ -22,7 +22,45 @@ Example 2:
 Input: nums = [1,2,3], k = 3
 Output: 2
 
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+
+//can't use sliding window, bc includes negative numbers
 */
+
+
+
+
+//optimized O(N) time, O(N) space solution: iterate through the array and keep track of prefix subarray sums in a hash map, starting with prefix: count, { 0: 1 }; the Map says that there is a contigious prefixed subarray that contains the values required to sum up to k up to that current index in the array
+//in each iteration:
+  //compute the currentSum
+  //compute the neededPrefixSum = currentSum - k ; the prefixSum needed to validate a contiguous subarray having the target sum k
+  //check the prefixSumMap to see if the neededPrefixSum exists, if so add its count to result count variable
+  //add the currentSum (ie. the prefix sum up to that point in the array) to the prefixSumMap
+var subarraySum = function(nums, k) {
+  var count = 0;
+  var currSum = 0;
+  var prefixMap = { 0: 1 };
+  for (var i = 0; i < nums.length; i++) {
+      currSum+=nums[i];
+      var reqPrefix = currSum - k;
+      if (reqPrefix in prefixMap) {
+          count+=prefixMap[reqPrefix];
+      }
+
+      if (prefixMap[currSum]) {
+          prefixMap[currSum]+=1
+      } else {
+          prefixMap[currSum] = 1;
+      }
+  }
+
+  return count;
+};
+
 
 
 //O(N^2) time limit exceeds
